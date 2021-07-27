c6ac9acdaf2ded148a1b5c0328b229958bbac7ec
setilanaji <setyajiyudha266@gmail.com>
2021-07-28 00:13:19 +0700

    Initial Commit

---
+++ interactive story ios example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  interactive story ios example
+//
+//  Created by Yudha S on 2021/7/28.
+//  Copyright © 2021 Macx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
